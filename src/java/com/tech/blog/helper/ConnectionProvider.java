6095c7aeaa65fa41e95e74fd93fdbae32c8e24b3
Dinesh chandra <dchandra10122000@gmail.com>
2022-12-24 04:34:03 +0530

    Project

---
+++ src/java/com/tech/blog/helper/ConnectionProvider.java
@@ -0,0 +1,32 @@
+
+package com.tech.blog.helper;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConnectionProvider {
+     private static Connection con;
+    public static Connection getConnection()
+    {
+        try
+        {
+           if(con==null)
+           {
+                //driver class load
+            
+            Class.forName("com.mysql.jdbc.Driver");
+            
+            //create a connection..
+            
+            con=DriverManager.getConnection("jdbc:mysql://3306/techblog", "system" , "dD4%lL6*xX7!");
+            
+           }
+        }catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+       
+        return con;
+    }
+    
+}
